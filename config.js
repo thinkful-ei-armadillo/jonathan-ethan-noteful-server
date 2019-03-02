04566a223d7e47233b6cc76c52838397373f17ee
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-03-01 19:43:20 -0500

    Fix name

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   port: 8000 || process.env.PORT,
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://noteful@localhost/noteful',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://noteful@localhost/test_notful',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://noteful@localhost/noteful',
+  TEST_DB_URL: process.env.TEST_DATABASE_URL || 'postgresql://noteful@localhost/test_notful',
   API_KEY: process.env.API_KEY
 };
